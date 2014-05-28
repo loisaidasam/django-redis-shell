e09ca760ac121a570ec492ee24c6fb628e9343ad
Sam Sandberg <sam.sandberg@gmail.com>
2014-05-28 12:36:01 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 django-redis-shell
 ==================
 
-./manage.py redisshell
+## Installation:
+
+    $ pip install git+git://github.com/loisaidasam/django-redis-shell.git
+
+
+## Usage:
+
+Shell access:
+
+    ./manage.py redisshell
+
+Read from shell:
+
+    ./manage.py redisshell keys '*'
+
+Delete from shell:
+
+    ./manage.py redisshell keys '*' | xargs ./manage.py redisshell del
---
+++ djangoredisshell/management/commands/redisshell.py
@@ -0,0 +1,42 @@
+
+from optparse import make_option
+import subprocess
+import urlparse
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    args = ""
+    help = "Get redis-cli command"
+
+    option_list = BaseCommand.option_list + (
+        make_option('-r', '--raw',
+            action='store_true',
+            dest='raw',
+            default=False,
+            help='Raw output'
+        ),
+    )
+
+    def handle(self, *args, **options):
+        parsed = urlparse.urlparse(settings.BROKER_URL)
+        cmd = "redis-cli "
+        if parsed.hostname:
+            cmd += "-h %s " % parsed.hostname
+        if parsed.port:
+            cmd += "-p %s " % parsed.port
+        if parsed.password:
+            cmd += "-a %s " % parsed.password
+        if options['raw']:
+            cmd += "--raw "
+        if args:
+            # Helper for handling stuff like ./manage.py redisshell keys '*'
+            if '*' in args:
+                args = list(args)
+                for i, arg in enumerate(args):
+                    if arg == '*':
+                        args[i] = "'*'"
+            cmd += " ".join(args)
+        subprocess.call(cmd, shell=True)
