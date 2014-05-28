b5fb2f574f1b1703faff311cfa13518c1027cce4
Sam Sandberg <sam.sandberg@gmail.com>
2014-05-28 12:46:13 -0400

    Added setup.py, license, etc.

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-redis-shell',
+    version='0.1',
+    packages=['djangoredisshell'],
+    include_package_data=True,
+    license='MIT License',
+    description='Command line support for ./manage.py redisshell',
+    long_description=README,
+    url='https://github.com/loisaidasam/django-redis-shell',
+    author='@LoisaidaSam',
+    author_email='sam.sandberg@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
